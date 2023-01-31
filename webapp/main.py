616809680e134f2a78e027fddec457da3339cbf1
RajSunner <lwp03rss@hotmail.com>
2023-01-31 16:45:42 +0000

    qa chain weaviate

--- webapp/main.py
+++ webapp/main.py
@@ -3,15 +3,22 @@ import base64
 import json
 from typing import Union, Any
 from os.path import dirname, abspath, join
-from fastapi import FastAPI, Request
+
+from functools import lru_cache
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from .chain import create_chain
 from .chain import create_qa_chain
-from .process import get_docs
+from .process import get_docs_w
+from .config import Settings
 
 app = FastAPI()
+
+@lru_cache()
+def get_settings():
+    return Settings()
 
 class Query(BaseModel):
     question: str
@@ -19,10 +26,10 @@ class Query(BaseModel):
     key: str
 
 @app.post("/llm")
-async def query(request: Request, query: Query):
+async def query(request: Request, query: Query, settings: Settings = Depends(get_settings)):
     # key = request.headers.get('X-Api-Key')
     key = query.key
-    docs = get_docs(query.question)
+    docs = get_docs_w(query.question, settings)
     chain = create_qa_chain(key)
     answer = chain.run(input_documents=docs, question=query.question)
     history = json.dumps(query.history)
--- webapp/process.py
+++ webapp/process.py
@@ -1,3 +1,4 @@
+import os
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
@@ -5,10 +6,28 @@ from langchain.docstore.document import Document
 from langchain.prompts import PromptTemplate
 import pickle
 from langchain.embeddings import HuggingFaceEmbeddings
-
+from langchain.vectorstores.weaviate import Weaviate
+import weaviate
 
 def get_docs(query):
     with open("webapp/data/report-q.pkl", 'rb') as f:
         new_docsearch = pickle.load(f)
     docs = new_docsearch.similarity_search(query)
+    return docs
+
+
+def get_docs_w(query, settings):
+
+    creds = weaviate.auth.AuthClientPassword(
+        username=settings.emailw, password=settings.passwordw)
+    client = weaviate.Client(
+        auth_client_secret=creds,
+        url=settings.urlw,
+        additional_headers={
+            'X-OpenAI-Api-Key': settings.okey
+        }
+    )
+    vectorstore = Weaviate(client, "Paragraph", "content")
+    query = "What went wrong?"
+    docs = vectorstore.similarity_search(query)
     return docs
