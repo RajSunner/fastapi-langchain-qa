5e58285d0117b59c8d58f31e1d9ce6293f787e37
RajSunner <lwp03rss@hotmail.com>
2023-02-10 17:26:28 +0000

    basic

--- webapp/main.py
+++ webapp/main.py
@@ -1,37 +1,52 @@
-import os
-import base64
 import json
-from typing import Union, Any
-from os.path import dirname, abspath, join
-
+from typing import Any
 from functools import lru_cache
 from fastapi import FastAPI, Request, Depends
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from .chain import create_chain
 from .chain import create_qa_chain
 from .process import get_docs_w
+from .process import create_vectors_from_url
 from .config import Settings
 
 app = FastAPI()
+
 
 @lru_cache()
 def get_settings():
     return Settings()
 
+
 class Query(BaseModel):
     question: str
-    history: Any
-    key: str
+
+
+class File(BaseModel):
+    question: str
+    file_url: str
+
 
 @app.post("/llm")
 async def query(request: Request, query: Query, settings: Settings = Depends(get_settings)):
-    # key = request.headers.get('X-Api-Key')
-    key = query.key
+    key = request.headers.get('X-Api-Key')
     docs = get_docs_w(query.question, settings)
     chain = create_qa_chain(key)
     answer = chain.run(input_documents=docs, question=query.question)
-    history = json.dumps(query.history)
-    results = {"answer": answer, "history": history}
+    # history = json.dumps(query.history)
+    results = {"message": answer}
+    return results
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/file")
+async def file_vec(request: Request, file: File, settings: Settings = Depends(get_settings)):
+    key = request.headers.get('X-Api-Key')
+    docs = create_vectors_from_url(file.file_url, file.question, settings)
+    chain = create_qa_chain(key)
+    answer = chain.run(input_documents=docs, question=file.question)
+    results = {"message": answer}
     return results
--- webapp/process.py
+++ webapp/process.py
@@ -1,4 +1,8 @@
 import os
+import time
+import urllib.request
+import PyPDF2
+import io
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
@@ -9,12 +13,94 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores.weaviate import Weaviate
 import weaviate
 
-def get_docs(query):
-    with open("webapp/data/report-q.pkl", 'rb') as f:
-        new_docsearch = pickle.load(f)
-    docs = new_docsearch.similarity_search(query)
-    return docs
+# def get_docs(query):
+#     with open("webapp/data/report-q.pkl", 'rb') as f:
+#         new_docsearch = pickle.load(f)
+#     docs = new_docsearch.similarity_search(query)
+#     return docs
 
+def create_vectors_from_url(url, query, settings):
+    URL = url
+    req = urllib.request.Request(URL)
+    remote_file = urllib.request.urlopen(req).read()
+    remote_file_bytes = io.BytesIO(remote_file)
+    reader = PyPDF2.PdfReader(remote_file_bytes)
+
+    report_text = ''
+
+    for x in range(len(reader.pages)):
+      page = reader.pages[x]
+      report_text += page.extract_text()
+
+    report_splitter = CharacterTextSplitter(separator=" ",chunk_size=1000, chunk_overlap=100)
+    texts_w = report_splitter.split_text(report_text)
+
+    creds = weaviate.auth.AuthClientPassword(
+        username=settings.emailw, password=settings.passwordw)
+    client = weaviate.Client(
+        auth_client_secret=creds,
+        url=settings.urlw,
+        additional_headers={
+            'X-OpenAI-Api-Key': settings.okey
+        }
+    )
+    client.schema.delete_all()
+    client.schema.get()
+    schema = {
+        "classes": [
+            {
+                "class": "Paragraph",
+                "description": "A written paragraph",
+                "vectorizer": "text2vec-openai",
+                "moduleConfig": {
+                    "text2vec-openai": {
+            "model": "ada",
+            "modelVersion": "002",
+            "type": "text"
+                    }
+                },
+                "properties": [
+                    {
+                        "dataType": ["text"],
+                        "description": "The content of the paragraph",
+                        "moduleConfig": {
+                            "text2vec-openai": {
+                            "skip": False,
+                            "vectorizePropertyName": False
+                            }
+                        },
+                        "name": "content",
+                    },
+                ],
+            },
+        ]
+    }
+    def callback(b):
+        time.sleep(
+            5
+        )
+    
+    client.batch(
+        batch_size=1,
+        dynamic=True,
+        creation_time=5,
+        timeout_retries=3,
+        connection_error_retries=3,
+        callback=callback,
+    
+    )
+
+    client.schema.create(schema)
+
+    with client.batch as batch:
+        for text in texts_w:
+            batch.add_data_object({"content": text}, "Paragraph")
+    
+    vectorstore = Weaviate(client, "Paragraph", "content")
+ 
+    docs = vectorstore.similarity_search(query)
+
+    return docs
 
 def get_docs_w(query, settings):
 
